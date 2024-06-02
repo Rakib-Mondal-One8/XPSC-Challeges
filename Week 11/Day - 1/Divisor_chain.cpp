49673c0e2e022c9a2d31bc55079918cf92c5930e
Rakib Mondal <143431829+RAKIBONE8@users.noreply.github.com>
2024-06-02 23:10:52 +0530

    Day - 1

---
+++ Week 11/Day - 1/Divisor_chain.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+typedef long long ll;
+#define pi pair<int, int>
+#define pll pair<ll, ll>
+#define Yes cout << "YES" << '\n'
+#define No cout << "NO" << '\n'
+#define nl '\n'
+#define loop(i, s, n) for (ll i = s; i <= n; i++)
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using pbdms = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+bool isPrime(ll n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (ll i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+ll lcm(ll a, ll b)
+{
+    return (a / __gcd(a, b)) * b;
+}
+int nXOR(int n)
+{
+    if (n % 4 == 0)
+        return n;
+    if (n % 4 == 1)
+        return 1;
+    if (n % 4 == 2)
+        return n + 1;
+    return 0;
+}
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+void solve()
+{
+    int x;
+    cin >> x;
+    vector<int> ans;
+    ans.push_back(x);
+    int lsb;
+    for (int i = 0; i <= 31; i++)
+    {
+        int tmp = x;
+        if ((1 << (int)log2(x)) == x)
+        {
+            lsb = log2(x);
+            break;
+        }
+        if ((tmp >> i) & 1)
+        {
+            x -= 1 << i;
+            ans.push_back(x);
+        }
+    }
+    while (lsb > 0)
+    {
+        x -= (1 << lsb - 1);
+        ans.push_back(x);
+        lsb--;
+    }
+    cout << ans.size() << nl;
+    for (int res : ans)
+    {
+        cout << res << " ";
+    }
+    cout << nl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
