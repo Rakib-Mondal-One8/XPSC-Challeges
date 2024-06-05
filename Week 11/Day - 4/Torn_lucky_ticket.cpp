2bd30f0e560103fcfa3684b055c40295a4c8fa62
Rakib Mondal <143431829+RAKIBONE8@users.noreply.github.com>
2024-06-05 22:55:25 +0530

    Day - 4

---
+++ Week 11/Day - 4/Torn_lucky_ticket.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+typedef long long ll;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pi pair<int, int>
+#define pll pair<ll, ll>
+#define Yes cout << "YES" << '\n'
+#define No cout << "NO" << '\n'
+#define nl '\n'
+#define loop(i, s, n) for (ll i = s; i <= n; i++)
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using pbdms = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/*Debug*/
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void init_code() {
+#ifndef ONLINE_JUDGE
+	freopen("Error.txt", "w", stderr);
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+}
+bool isPrime(ll n) {
+	if (n <= 1)
+		return false;
+	if (n <= 3)
+		return true;
+	if (n % 2 == 0 || n % 3 == 0)
+		return false;
+	for (ll i = 5; i * i <= n; i += 6) {
+		if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+	}
+	return true;
+}
+ll lcm(ll a, ll b) {
+	return (a / __gcd(a, b)) * b;
+}
+int nXOR(int n) {
+	if (n % 4 == 0)
+		return n;
+	if (n % 4 == 1)
+		return 1;
+	if (n % 4 == 2)
+		return n + 1;
+	return 0;
+}
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+bool cmp(string &a, string &b) {
+	return a.size() < b.size();
+}
+void solve()
+{
+	ll n;
+	cin >> n;
+	vector<string>a(n);
+	loop(i, 0, n - 1)cin >> a[i];
+	sort(a.begin(), a.end(), cmp);
+	map<pll, ll>freq;
+	ll ans  = n;
+	for (auto s : a) {
+		ll n = s.size();
+		loop(m, 1, n) {
+			if ((n + m) % 2 == 1)continue;
+			ll mid = (n + m) / 2;
+			ll req = 0;
+			loop(i, 0, mid - 1) req += (s[i] - '0');
+			loop(i, mid, n - 1) req -= (s[i] - '0');
+			if (req >= 0) ans += freq[ {m, req}];
+		}
+		reverse(s.begin(), s.end());
+		loop(m, 1, n) {
+			if ((n + m) % 2 == 1)continue;
+			ll mid = (n + m) / 2;
+			ll req = 0;
+			loop(i, 0, mid - 1) req += (s[i] - '0');
+			loop(i, mid, n - 1) req -= (s[i] - '0');
+			if (req >= 0) ans += freq[ {m, req}];
+		}
+		ll sum = 0;
+		loop(i, 0, n - 1) sum += (s[i] - '0');
+		freq[ {n, sum}]++;
+	}
+	cout << ans << nl;
+}
+int main()
+{
+	init_code();
+	fastio();
+	solve();
+	return 0;
+}
+
