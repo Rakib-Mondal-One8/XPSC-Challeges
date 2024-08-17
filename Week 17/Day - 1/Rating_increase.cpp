027f486598858ba0f911df2400af982ab43913fb
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2024-08-17 12:53:33 +0530

    Day - 1

---
+++ Week 17/Day - 1/Rating_increase.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+/*Problem Link -> https://codeforces.com/contest/1913/problem/A*/
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pi pair<int, int>
+#define pll pair<ll, ll>
+#define Yes cout << "YES" << '\n'
+#define No cout << "NO" << '\n'
+#define nl '\n'
+#define loop(i, s, n) for (ll i = s; i <= n; i++)
+using namespace std;
+using namespace __gnu_pbds;
+using namespace chrono;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using pbdms = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/*Debug*/
+#ifdef Rakib_18
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void init_code() {
+#ifdef Rakib_18
+	freopen("Error.txt", "w", stderr);
+#endif
+}
+const ll mod = 1e9 + 7;
+ll inv(ll i) {if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;}
+bool isPrime(ll n) {if (n <= 1)return false; if (n <= 3)return true; if (n % 2 == 0 || n % 3 == 0)return false; for (ll i = 5; i * i <= n; i += 6) {if (n % i == 0 || n % (i + 2) == 0)return false;} return true;}
+ll lcm(ll a, ll b) {return (a / __gcd(a, b)) * b;}
+int nXOR(int n) {if (n % 4 == 0)return n; if (n % 4 == 1)return 1; if (n % 4 == 2)return n + 1; return 0;}
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+void solve()
+{
+	string s;
+	cin >> s;
+	int i = 1;
+	while (s[i] - '0' == 0 && i < s.size())i++;
+	string l = s.substr(0, i);
+	string r = s.substr(i, s.size() - 1);
+	auto ok = [&](string res) {
+		int p = 0, len = res.size();
+		ll val = 0;
+		while (p < len) {
+			val = val * 10 + (res[p] - '0');
+			p++;
+		}
+		return val;
+	};
+	ll a = ok(l);
+	ll b = ok(r);
+	if (b > a)cout << a << " " << b << nl;
+	else cout << -1 << nl;
+}
+int main()
+{
+	init_code();
+	fastio();
+	int t = 1;
+	cin >> t;
+	auto start1 = high_resolution_clock::now();
+	while (t--)
+	{
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef Rakib_18
+	cerr << "Time: " << duration . count() / 1000 << " ms" << endl;
+#endif
+	return 0;
+}
